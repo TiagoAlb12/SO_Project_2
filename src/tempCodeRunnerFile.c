c30aff69e3fcd8032da5f55e2b246861b9300e94
Tiago Albuquerque <120564959+TiagoAlb12@users.noreply.github.com>
2023-12-26 17:12:01 +0000

    Add files via upload

---
+++ src/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+/**
+ *  \brief chef cooks, then delivers the food to the waiter 
+ *
+ *  The chef takes some time to cook and signals the waiter that food is 
+ *  ready (this may only happen when waiter is available)
+ *  then updates its state.
+ *  The internal state should be saved.
+ */
